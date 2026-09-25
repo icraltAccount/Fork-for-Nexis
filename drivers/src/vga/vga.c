cdedfc427f8066f165eda5e41c144ea2bab0f973
Henrique Santos <hiqkrs@gmail.com>
2026-09-25 15:14:33 -0300

    refactor(drivers): polished vga driver

--- drivers/src/vga/vga.c
+++ drivers/src/vga/vga.c
@@ -1,4 +1,5 @@
 #include "vga/vga.h"
+#include "io/io.h"
 
 static volatile vga_cell_t *const framebuffer = (volatile vga_cell_t *)VGA_MEMORY;
 
@@ -10,21 +11,27 @@ vga_cell_t vga_make_cell(vga_char_t c, vga_attr_t attr) {
     return (vga_cell_t)(((vga_cell_t)attr << 8) | (vga_char_t)c);
 }
 
-vga_coord_t vga_get_index(vga_coord_t x, vga_coord_t y) {
+vga_index_t vga_get_index(vga_coord_t x, vga_coord_t y) {
     if (x >= VGA_WIDTH) x = VGA_WIDTH - 1;
     if (y >= VGA_HEIGHT) y = VGA_HEIGHT - 1;
-
-    return (vga_coord_t)(y * VGA_WIDTH + x);
+    return (vga_index_t)(y * VGA_WIDTH + x);
 }
 
-void vga_set_cell(vga_cell_t cell, vga_coord_t index) {
+void vga_set_cell(vga_cell_t cell, vga_index_t index) {
     if (index >= VGA_WIDTH * VGA_HEIGHT) return;
-
     framebuffer[index] = cell;
 }
 
-vga_cell_t vga_get_cell(vga_coord_t index) {
+vga_cell_t vga_get_cell(vga_index_t index) {
     if (index >= VGA_WIDTH * VGA_HEIGHT) return 0;
-
     return framebuffer[index];
+}
+
+void vga_move_cursor(vga_coord_t x, vga_coord_t y) {
+    vga_index_t pos = vga_get_index(x, y);
+
+    io_outb(0x3D4, 0x0E);
+    io_outb(0x3D5, (pos >> 8) & 0xFF);
+    io_outb(0x3D4, 0x0F);
+    io_outb(0x3D5, pos & 0xFF);
 }
--- lib/include/io/io.h
+++ lib/include/io/io.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
-void outb(uint16_t port, uint8_t val);
-uint8_t inb(uint16_t port);
-void io_wait(void);
+typedef uint16_t io_port_t;
+typedef uint8_t io_value_t;
+
+void io_outb(io_port_t port, io_value_t val);
+uint8_t io_inb(io_port_t port);
